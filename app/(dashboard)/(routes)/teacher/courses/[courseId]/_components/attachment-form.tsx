28051d0792cc40ea0f5a8a204a143ed4a38640f3
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-09-04 07:24:21 +0530

    feat: course attachment form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { FileUpload } from "@/components/file-upload";
+import { Button } from "@/components/ui/button";
+import { Attachment, Course } from "@prisma/client";
+import axios from "axios";
+import { File, Loader2, PlusCircle, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+interface AttachmentFormProps {
+  initialData: Course & { attachments: Attachment[] };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  url: z.string().min(1, { message: "Url is required" }),
+});
+
+export const AttachmentForm = ({
+  initialData,
+  courseId,
+}: AttachmentFormProps) => {
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post(`/api/courses/${courseId}/attachments`, values);
+      toast.success("Course attachment added successfully!");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("Failed to add course attachments. Something went wrong.");
+      console.error(error);
+    }
+  };
+
+  const onDelete = async (attachmentId: string) => {
+    try {
+      setDeletingId(attachmentId);
+      await axios.delete(
+        `/api/courses/${courseId}/attachments/${attachmentId}`
+      );
+      toast.success("Course attachment deleted successfully!");
+      router.refresh();
+    } catch (error) {
+      toast.error("Failed to delete course attachment. Something went wrong.");
+      console.error(error);
+    } finally {
+      setDeletingId(null);
+    }
+  };
+
+  const toggleEdit = () => setIsEditing(!isEditing);
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course attachments
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add a file
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className="text-sm mt-2 text-slate-500 italic">
+              No attachments yet
+            </p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+                >
+                  <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                  <p className="text-xs line-clamp-1">{attachment.name}</p>
+                  {deletingId === attachment.id && (
+                    <div>
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    </div>
+                  )}
+                  {deletingId !== attachment.id && (
+                    <button
+                      onClick={() => onDelete(attachment.id)}
+                      className="ml-auto hover:opacity-75 transition"
+                    >
+                      <X className="h-4 w-4" />
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      )}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseAttachment"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ url: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            Add anything your students might need to complete the course.
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
