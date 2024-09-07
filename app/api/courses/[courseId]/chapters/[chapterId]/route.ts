7fd801b142b8742c935f326c4657a2bf59663200
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-09-07 10:55:36 +0530

    feat:chapter title form

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isPublished, ...values } = await request.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const chapter = await db.chapter.update({
+      where: { id: params.chapterId, courseId: params.courseId },
+      data: values,
+    });
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.error(`[COURSES_CHAPTER_ID]`, error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
