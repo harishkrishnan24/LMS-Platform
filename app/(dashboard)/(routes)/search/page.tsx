b65362f38d8198db9fb8ffca7d09f72b05888c2e
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-10-05 15:10:32 +0530

    feat: list categories

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,3 +1,15 @@
-export default function SearchPage() {
-  return <div>SearchPage</div>;
+import { db } from "@/lib/db";
+import { Categories } from "./_components/categories";
+
+export default async function SearchPage() {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 }
