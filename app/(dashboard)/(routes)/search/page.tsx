12cd25d056c54619d85d7cc12a61088749dba4bc
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-10-06 11:18:18 +0530

    feat: list published courses

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,20 +1,40 @@
-import { db } from "@/lib/db";
-import { Categories } from "./_components/categories";
+import { getCourses } from "@/actions/get-courses";
 import { SearchInput } from "@/components/search-input";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { Categories } from "./_components/categories";
+import { CoursesList } from "@/components/courses-list";
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
+  const courses = await getCourses({ userId, ...searchParams });
+
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
