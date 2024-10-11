0f3192c4d3ec4b5fc2854c7e696abc0547db2c4d
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-10-11 11:04:21 +0530

    feat: protect teacher mode

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,12 +1,13 @@
+import { isTeacher } from "@/lib/teacher";
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
 const handleAuth = () => {
   const { userId } = auth();
-  if (!userId) {
+  const isAuthorized = isTeacher(userId);
+  if (!userId || !isAuthorized) {
     throw new Error("Unauthorized");
   }
   return { userId };
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { isTeacher } from "@/lib/teacher";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 import { SearchInput } from "./search-input";
+import { Button } from "./ui/button";
 
 export default function NavbarRoutes() {
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isCoursePage = pathname?.startsWith("/courses");
@@ -29,13 +31,13 @@ export default function NavbarRoutes() {
               Exit
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher Mode
             </Button>
           </Link>
-        )}
+        ) : null}
         <UserButton afterSwitchSessionUrl="/" />
       </div>
     </>
